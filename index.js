1b2fed77b592aebe68845bdea96801c1a3c688ca
tuvalp <72265289+tuvalp@users.noreply.github.com>
2021-01-28 22:36:02 +0200

    a

--- index.js
+++ index.js
@@ -33,9 +33,7 @@ mongoose.connect(
 
   app.use(express.static( 'client/build' ));
 
-  app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
-  });
+
 
 
 // set up routes
